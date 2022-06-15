51d366f98512571c6b8dbeb691a87e6754bae5bb
Eduard <erafegas@gmail.com>
2022-06-15 17:34:08 +0200

    Exercici 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import Escena from './components/escena/Escena';
+import styled from 'styled-components';
+
+
+const Pagina = styled.div `
+display: flex;
+width: 100%;
+height: 100vh;
+flex-direction: column;
+align-items: center;
+`;
+
 
 function App() {
   return (
-      <Escena />
+        <div> 
+          <Pagina>
+            <Escena />
+         </Pagina>
+        </div>
   );
 }
 
--- src/components/escena/Escena.js
+++ src/components/escena/Escena.js
@@ -1,18 +1,31 @@
 import React from 'react'
 import Frases from './Frases';
+import styled from 'styled-components';
+
+const Section = styled.div `
+   margin: 25px;
+   width: 95%;
+   border: 1px solid #000;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   padding: 25px;
+   border-radius: 20px;
+   `;
+
 
 const Escena = () => {
 
     // Lógica
     const frases = Frases.map((props, i) => (
-        <p> {props.txt} </p>
+        <Section> {props.txt} </Section>
         ));
 
     // Vista
   return (
-    <div>
+    <>
      {frases}
-    </div>
+    </>
   );
 
 }
