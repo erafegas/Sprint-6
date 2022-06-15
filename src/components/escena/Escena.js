80d2ab6ea82c90221d6e90937e95f38d6ccc8ad3
Eduard <erafegas@gmail.com>
2022-06-15 13:34:18 +0200

    Exercici 1

---
+++ src/components/escena/Escena.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Escena = () => {
+
+const frase =  <p> El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial </p>;
+
+  return (
+    <div>
+      {frase}
+    </div>
+  )
+}
+
+export default Escena
